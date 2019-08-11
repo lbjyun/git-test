ed4d2fe68aa47a9665e01809551535703d652bfb
wangwu <wangwu@qq.com>
2019-08-11 09:33:46 +0800

    aa

--- src/com/git/test.java
+++ src/com/git/test.java
@@ -19,5 +19,14 @@ public class test {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+
+
+
+        int i=0;
+        int j=1;
+        System.out.println(i+j);
+
     }
+
+
 }
